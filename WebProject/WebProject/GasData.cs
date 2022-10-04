c6d6b2a44bc5c337210a937f65b8e14f271997a5
KasparasCiurlionis <kasparasciurlionis@gmail.com>
2022-10-04 16:49:50 +0300

    manualy added table branch

---
+++ WebProject/WebProject/GasData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace WebProject
+{
+    public class GasData
+    {
+        public static List<GasStation> getData()
+        {
+            string path = System.AppDomain.CurrentDomain.BaseDirectory+"/App_Data/Data";
+            List<GasStation> data=new List<GasStation>();
+            foreach (string file in Directory.EnumerateFiles(path, "*.txt"))
+            {
+                string contents = File.ReadAllText(file);
+                string name = (Path.GetFileNameWithoutExtension(file) + " ");
+                string[] locations = contents.Split('\n');  
+                data.Add(new GasStation(name, locations)); 
+            }
+            return data;
+        }
+    }
+}
