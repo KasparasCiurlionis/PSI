2b8ba61a93c0740db937039333340dcb74f965e4
unknown <Olgierd199>
2022-10-13 17:43:58 +0300

    Changes to pull

--- WebProject/WebProject/Presentation/Insertion.aspx.cs
+++ WebProject/WebProject/Presentation/Insertion.aspx.cs
@@ -18,6 +18,7 @@ namespace WebProject
 {
     public partial class Insertion : System.Web.UI.Page
     {
+        // Olgierd change
         private string current_location;
         private string chosenGasStation;
 
