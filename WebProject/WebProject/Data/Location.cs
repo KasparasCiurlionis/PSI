92c44fbc397eb5693e4312446ebb19d320322533
KasparasCiurlionis <kasparasciurlionis@gmail.com>
2022-10-16 21:55:55 +0300

    reworked the gasStation class, created the GasStations Ienumerable class, Location class and Coords struct, remade gasData and processData to work with these changes

---
+++ WebProject/WebProject/Data/Location.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace WebProject
+{
+    public class Location
+    {
+        Coords coords;
+        public Location(Coords coords = new Coords())
+        {
+            this.coords = coords;
+        }
+
+        public Coords getCoords()
+        {
+            return coords;
+        }
+
+        public void setCoords(Coords coords)
+        {
+            this.coords = coords;
+        }
+    }
+
+    public struct Coords
+    {
+        public Coords(double x=0, double y=0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+
+        public override string ToString() => $"({X}, {Y})";
+    }
+}
