82f0fbe52a75d59853cff1da895062f6276a9dab
KasparasCiurlionis <kasparasciurlionis@gmail.com>
2022-11-28 20:25:25 +0200

    generic event exception logger

--- WebProject/WebProject/Data/ExceptionLogger.cs
+++ WebProject/WebProject/Data/ExceptionLogger.cs
@@ -5,13 +5,16 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WebProject.Data
 {
     public class ExceptionLogger
     {
         public static async Task log<T>(T text) {
-            await logAsync<T>(text);
+            ProcessLogger<T> bl = new ProcessLogger<T>();
+            bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            bl.StartProcess(text);
         }
         static Task logAsync<T>(T text)
         {
@@ -31,6 +34,27 @@ namespace WebProject.Data
                 sw.WriteLine(text.ToString());
 
             }
-        } 
+        }
+
+        public static async void bl_ProcessCompleted<T>(object sender, T e)
+        {
+            await logAsync<T>(e);
+        }
+    }
+
+    public class ProcessLogger<T>
+    {
+        // declaring an event using built-in EventHandler
+        public event EventHandler<T> ProcessCompleted;
+
+        public void StartProcess(T text)
+        {
+                OnProcessCompleted(text);
+        }
+
+        protected virtual void OnProcessCompleted(T text)
+        {
+            ProcessCompleted?.Invoke(this, text);
+        }
     }
 }
